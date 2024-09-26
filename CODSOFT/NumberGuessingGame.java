fee04b5fb6d9096f6a1841ed4a81219130a64f06
KavyaPriya <kavyapriya2210@gmail.com>
2024-09-26 23:37:52 +0530

    second commit

--- CODSOFT/NumberGuessingGame.java
+++ CODSOFT/NumberGuessingGame.java
@@ -1,6 +1,5 @@
 import java.util.Random;
 import java.util.Scanner;
-
 public class NumberGuessingGame {
     public static void main(String[] args) {
         Random rand = new Random(); // random number generator
